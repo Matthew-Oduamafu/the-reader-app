f096fb3b71fdbed987be2e767c5e57aa40df84e9
Matthew Oduamafu <72637895+Matthew-Oduamafu@users.noreply.github.com>
2022-06-11 21:34:26 +0000

    3nd Commit AppModule & ReaderApplication super class added and Manifest configuration done

--- app/src/main/java/mattie/freelancer/reaader/MainActivity.kt
+++ app/src/main/java/mattie/freelancer/reaader/MainActivity.kt
@@ -1,6 +1,7 @@
 package mattie.freelancer.reaader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,13 +11,31 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.AndroidEntryPoint
 import mattie.freelancer.reaader.ui.theme.ReaaderTheme
 
+private const val TAG = "MainActivity"
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContent {
             ReaaderTheme {
+                val db = FirebaseFirestore.getInstance()
+                val user: MutableMap<String, Any> = HashMap()
+                user["fName"] = "Mattie"
+                user["lName"] = "Oduamafu"
+
+                with(db.collection("users")) {
+                    add(user).addOnSuccessListener {
+                        Log.d(TAG, "onCreate: users:: ${it.id} saved")
+                    }.addOnFailureListener {
+                        Log.d(TAG, "onCreate: users:: $it failed to save")
+                    }
+                }
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
